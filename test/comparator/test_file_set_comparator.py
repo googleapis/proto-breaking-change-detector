8ed1a2950c2ba16d76fe6aa2b176573e53f7886e
Xiaozhen Liu <xiaozhenliu@google.com>
2020-10-21 16:05:45 -0700

    feat: detect services change in file-level comparator (#15)
    * service change in file-level
    
    * fix unittest
    
    * clean up
    
    * feedback

---
+++ test/comparator/test_file_set_comparator.py
@@ -0,0 +1,37 @@
+import unittest
+from test.tools.invoker import UnittestInvoker
+from src.comparator.file_set_comparator import FileSetComparator
+from src.findings.finding_container import FindingContainer
+from src.findings.utils import FindingCategory
+
+
+class FileSetComparatorTest(unittest.TestCase):
+    # This is for tesing the behavior of src.comparator.service_comparator.ServiceComparator class.
+    # UnittestInvoker helps us to execute the protoc command to compile the proto file,
+    # get a *_descriptor_set.pb file (by -o option) which contains the serialized data in protos, and
+    # create a FileDescriptorSet (_PB_ORIGNAL and _PB_UPDATE) out of it.
+
+    def tearDown(self):
+        FindingContainer.reset()
+
+    def test_service_change(self):
+        _INVOKER_ORIGNAL = UnittestInvoker(
+            ["service_v1.proto"], "service_v1_descriptor_set.pb"
+        )
+        _INVOKER_UPDATE = UnittestInvoker(
+            ["service_v1beta1.proto"], "service_v1beta1_descriptor_set.pb"
+        )
+        FileSetComparator(_INVOKER_ORIGNAL.run(), _INVOKER_UPDATE.run()).compare()
+        findings_map = {f.message: f for f in FindingContainer.getAllFindings()}
+        self.assertEqual(
+            findings_map[
+                "The paginated response of method paginatedMethod is changed"
+            ].category.name,
+            "METHOD_PAGINATED_RESPONSE_CHANGE",
+        )
+        _INVOKER_ORIGNAL.cleanup()
+        _INVOKER_UPDATE.cleanup()
+
+
+if __name__ == "__main__":
+    unittest.main()
