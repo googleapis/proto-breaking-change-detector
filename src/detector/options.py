8b00c749b2ca55664677109d23b8287a09fe2ca9
Xiaozhen Liu <xiaozhenliu@google.com>
2020-11-13 17:08:40 +0000

    add options builder

---
+++ src/detector/options.py
@@ -0,0 +1,54 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+
+class Options:
+    # Build the options for protoc command arguments.
+    def __init__(
+        self,
+        proto_dirs_original: str,
+        proto_dirs_update: str,
+        package_prefixes: str = None,
+        human_readable_message: bool = False,
+        output_json_path: str = None,
+    ):
+        self.proto_dirs_original = self._get_proto_dirs(proto_dirs_original)
+        self.proto_dirs_update = self._get_proto_dirs(proto_dirs_update)
+        self.package_prefixes = self._get_package_prefixes(package_prefixes)
+        self.human_readable_message = human_readable_message
+        self.output_json_path = self._get_output_json_path(output_json_path)
+
+    def _get_proto_dirs(self, proto_dirs):
+        proto_dirs_arr = proto_dirs.split(",")
+        if not proto_dirs_arr:
+            return []
+        for directory in proto_dirs_arr:
+            if not os.path.isdir(directory):
+                raise TypeError(f"The directory {directory} is not existing.")
+        return proto_dirs_arr
+
+    def _get_package_prefixes(self, prefixes):
+        if not prefixes:
+            return None
+
+        return [prefix.strip() for prefix in prefixes.split(",")]
+
+    def _get_output_json_path(self, path):
+        if not path:
+            return os.path.join(os.getcwd(), "detected_breaking_changes.json")
+        elif not os.isfile(path):
+            raise TypeError(f"The output_json_path {path} is not existing.")
+        return path
