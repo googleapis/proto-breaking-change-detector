39b42ea1d37ffdd5131f5af164010feed1b4b925
Xiaozhen Liu <xiaozhenliu@google.com>
2020-11-16 05:39:42 +0000

    command line interface

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='proto-breaking-change-detector',
-    version='0.1',
-    py_modules=['proto-breaking-change-detector'],
+    name="proto-breaking-change-detector",
+    version="0.1",
+    py_modules=["proto-breaking-change-detector"],
     install_requires=[
-        'Click',
+        "Click",
+        "protobuf >= 3.12.0",
+        "google-api-core >= 1.17.0",
+        "googleapis-common-protos >= 1.6.0",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         proto-breaking-change-detector=src.cli.detect:detect
-    ''',
+    """,
 )
--- src/cli/detect.py
+++ src/cli/detect.py
@@ -14,16 +14,19 @@
 
 import click
 import os
+from src.detector.options import Options
+from src.detector.loader import Loader
+from src.detector.detector import Detector
 
 
 @click.command()
 @click.argument(
     "original",
-    # help="The directories for the original version proto API definition files, separate by commas.",
+    help="The directories for the original version proto API definition files, separate by commas.",
 )
 @click.argument(
     "update",
-    # help="The directories for the updated version proto API definition files, separate by commas.",
+    help="The directories for the updated version proto API definition files, separate by commas.",
 )
 @click.option(
     "--output_json_path",
@@ -32,14 +35,14 @@ import os
 )
 @click.option(
     "--package_prefixes",
-    default='',
-    help="The file path for json output which contains all the breaking change findings. The default path is the current folder.",
+    default="",
+    help="The prefixes for the package name of the API definition proto files.",
 )
 @click.option(
     "--human_readable_message",
     default=False,
     is_flag=True,
-    help="Enable the human-readable message output if set to True.",
+    help="Enable the human-readable message output if set to True. Default value is false.",
 )
 def detect(
     original: str,
@@ -48,21 +51,22 @@ def detect(
     package_prefixes: str,
     human_readable_message: bool,
 ):
+    """Detect the breaking changes of the original and updated versions of API definition files. """
     print(original)
     print(update)
     print(output_json_path)
     print(human_readable_message)
-
-
-# 1. Readin the stdin options including [required] original_API_definition_dirs,
-# update_API_definition_dirs, [optional] human-readable-message, output_json_file.
-# 2. Create the options for all the command args. (options.Options.build())
-
-# 3. Create protoc command (back up solution) to load the FileDescriptorSet.(loader.py)
-# It takes options, returns fileDescriptorSet.
-
-# 4. Create detector with two FileDescriptorSet and options.
-# It returns output_json file and human-readable message based on options.
+    # 1. Readin the stdin options and create the Options object for all the command args.
+    options = Options(
+        original, update, package_prefixes, human_readable_message, output_json_path
+    )
+    # 3. Create protoc command (back up solution) to load the FileDescriptorSet.(loader.py)
+    # It takes options, returns fileDescriptorSet.
+    file_set_original = Loader(options.proto_dirs_original).get_descriptor_set()
+    file_set_update = Loader(options.proto_dirs_update).get_descriptor_set()
+    # 4. Create detector with two FileDescriptorSet and options.
+    # It returns output_json file and human-readable message based on options.
+    Detector(file_set_original, file_set_update, options).detect_breaking_changes()
 
 
 if __name__ == "__main__":
