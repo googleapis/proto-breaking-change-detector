792b7ac5223b0ca9b03cf21202995b4e171ca2ff
Xiaozhen Liu <xiaozhenliu@google.com>
2020-11-16 11:29:07 -0800

    feat: [cli][2/4] cli options builder (#64)
    * move invoker to detector
    
    * add unit tests
    
    * clean up
    
    * add options builder
    
    * add unit test
    
    * clean up
    
    * feedback
    
    * clean up
    
    * clean

---
+++ src/detector/options.py
@@ -0,0 +1,65 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+
+class Options:
+    """Build the options for protoc command arguments.
+
+    proto_dirs: Required. The directories where we should find the proto files,
+                including proto definition files and their dependencies.
+                Comma separated string.
+    package_prefixes: Optional.The package prefixes of the proto definition files,
+                      so that in the comparators, we can safely skip the
+                      dependency protos if needed. Comma separated string.
+    human_readable_message: Optional flag. Enable printing the human-readable
+                            messages if true. Default value if false.
+    output_json_path: Optional. The path of the findings json file. If not specify,
+                      we will create a json for the users which is in
+                      `$root/detected_breaking_changes.json`.
+    """
+
+    def __init__(
+        self,
+        proto_dirs_original: str,
+        proto_dirs_update: str,
+        package_prefixes: str = None,
+        human_readable_message: bool = False,
+        output_json_path: str = None,
+    ):
+        self.proto_dirs_original = self._get_proto_dirs(proto_dirs_original)
+        self.proto_dirs_update = self._get_proto_dirs(proto_dirs_update)
+        self.package_prefixes = self._get_package_prefixes(package_prefixes)
+        self.human_readable_message = human_readable_message
+        self.output_json_path = self._get_output_json_path(output_json_path)
+
+    def _get_proto_dirs(self, proto_dirs):
+        proto_dirs_arr = proto_dirs.split(",")
+        for directory in proto_dirs_arr:
+            if not os.path.isdir(directory):
+                raise TypeError(f"The directory {directory} is not existing.")
+        return proto_dirs_arr
+
+    def _get_package_prefixes(self, prefixes):
+        if not prefixes:
+            return None
+        return [prefix.strip() for prefix in prefixes.split(",")]
+
+    def _get_output_json_path(self, path):
+        if not path:
+            return os.path.join(os.getcwd(), "detected_breaking_changes.json")
+        elif not os.path.isfile(path):
+            raise TypeError(f"The output_json_path {path} is not existing.")
+        return path
---
+++ test/detector/test_options.py
@@ -0,0 +1,82 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from unittest import mock
+import os
+from src.detector.options import Options
+
+
+class OptionsTest(unittest.TestCase):
+    def test_options_default(self):
+        with mock.patch("os.path.isdir") as mocked_isdir:
+            mocked_isdir.return_value = True
+            opts = Options(
+                proto_dirs_original="c,d",
+                proto_dirs_update="a,b",
+            )
+        self.assertEqual(opts.proto_dirs_original, ["c", "d"])
+        self.assertEqual(opts.proto_dirs_update, ["a", "b"])
+        # Default value for human_readable_message is False.
+        self.assertFalse(opts.human_readable_message)
+        # Package_prefixes should be None if not set, no
+        # external dependencie is needed.
+        self.assertEqual(opts.package_prefixes, None)
+        # Use default json path if not set.
+        self.assertEqual(
+            opts.output_json_path,
+            os.path.join(os.getcwd(), "detected_breaking_changes.json"),
+        )
+
+    def test_options_basic(self):
+        with mock.patch("os.path.isdir") as mocked_isdir:
+            mocked_isdir.return_value = True
+            opts = Options(
+                proto_dirs_original="c,d",
+                proto_dirs_update="a,b",
+                human_readable_message=True,
+                package_prefixes="prefix1, prefix2, prefix3",
+            )
+        self.assertEqual(opts.proto_dirs_original, ["c", "d"])
+        self.assertEqual(opts.proto_dirs_update, ["a", "b"])
+        self.assertTrue(opts.human_readable_message)
+        # Strip the unneeded whitespaces.
+        self.assertEqual(opts.package_prefixes, ["prefix1", "prefix2", "prefix3"])
+        # Use default json path if not set.
+        self.assertEqual(
+            opts.output_json_path,
+            os.path.join(os.getcwd(), "detected_breaking_changes.json"),
+        )
+
+    def test_options_directory_not_existing(self):
+        with self.assertRaises(TypeError):
+            # The directory is not existing, raise TypeError.
+            Options(
+                proto_dirs_original="c,d",
+                proto_dirs_update="a,b",
+                human_readable_message=True,
+                package_prefixes="prefix1, prefix2, prefix3",
+            )
+
+    def test_options_json_file_not_existing(self):
+        with self.assertRaises(TypeError):
+            with mock.patch("os.path.isdir") as mocked_isdir:
+                mocked_isdir.return_value = True
+                # The output json file is not existing, raise TypeError.
+                Options(
+                    proto_dirs_original="c,d",
+                    proto_dirs_update="a,b",
+                    human_readable_message=True,
+                    output_json_path="not_existing",
+                )
