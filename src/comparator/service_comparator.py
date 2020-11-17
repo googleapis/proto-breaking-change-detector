f219a79bb99865b357a755e2cc25cf95f8bf9a34
Xiaozhen Liu <xiaozhenliu@google.com>
2020-11-17 11:00:42 -0800

    test: add integration tests (#72)
    * setup
    
    * move invoker to detector
    
    * add unit tests
    
    * clean up
    
    * add options builder
    
    * add unit test
    
    * clean up
    
    * main detector setup
    
    * feedback
    
    * clean up
    
    * add unit test
    
    * clean up
    
    * clean
    
    * command line interface
    
    * test
    
    * clean up
    
    * format
    
    * clean
    
    * integration test for cli
    
    * add cli test to Github Actions
    
    * fix messages
    
    * integration tests
    
    * fix tests
    
    * fix tests

--- src/comparator/service_comparator.py
+++ src/comparator/service_comparator.py
@@ -138,14 +138,20 @@ class ServiceComparator:
                     actionable=True,
                 )
             # 3.7 The paginated response of an RPC method is changed.
-            if method_original.paged_result_field != method_update.paged_result_field:
-                FindingContainer.addFinding(
-                    category=FindingCategory.METHOD_PAGINATED_RESPONSE_CHANGE,
-                    proto_file_name=method_update.proto_file_name,
-                    source_code_line=method_update.source_code_line,
-                    message=f"The paginated response of an existing method `{name}` is changed.",
-                    actionable=True,
-                )
+            if method_original.paged_result_field or method_update.paged_result_field:
+                if (
+                    not method_original.paged_result_field
+                    or not method_update.paged_result_field
+                    or method_original.paged_result_field.name
+                    != method_update.paged_result_field.name
+                ):
+                    FindingContainer.addFinding(
+                        category=FindingCategory.METHOD_PAGINATED_RESPONSE_CHANGE,
+                        proto_file_name=method_update.proto_file_name,
+                        source_code_line=method_update.source_code_line,
+                        message=f"The paginated response of an existing method `{name}` is changed.",
+                        actionable=True,
+                    )
             # 3.8 The method_signature annotation is changed.
             self._compare_method_signatures(method_original, method_update)
 
--- test/cli/test_detect.py
+++ test/cli/test_detect.py
@@ -20,7 +20,7 @@ class CliDetectTest(unittest.TestCase):
     def tearDown(self):
         FindingContainer.reset()
 
-    def test_single_directory(self):
+    def test_single_directory_enum(self):
         # Mock the stdout so that the unit test does not
         # print anything to the console.
         with patch("sys.stdout", new=StringIO()):
@@ -28,14 +28,84 @@ class CliDetectTest(unittest.TestCase):
             result = runner.invoke(
                 detect,
                 [
-                    "test/testdata/protos/example/enum_v1",
-                    "test/testdata/protos/example/enum_v1beta1",
+                    "test/testdata/protos/enum/v1",
+                    "test/testdata/protos/enum/v1beta1",
                     "--human_readable_message",
                 ],
             )
             self.assertEqual(result.exit_code, 0)
             self.assertEqual(
                 result.output, "enum_v1.proto L5: An Enum `BookType` is removed.\n"
+            )
+
+    def test_single_directory_message(self):
+        with patch("sys.stdout", new=StringIO()):
+            runner = CliRunner()
+            result = runner.invoke(
+                detect,
+                [
+                    "test/testdata/protos/message/v1",
+                    "test/testdata/protos/message/v1beta1",
+                    "--human_readable_message",
+                ],
+            )
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                "message_v1beta1.proto L7: Type of an existing field `id` is changed from `TYPE_INT32` to `TYPE_STRING`.\n"
+                + "message_v1beta1.proto L8: Name of an existing field is changed from `email` to `email_address`.\n"
+                + "message_v1beta1.proto L21: Repeated state of an existing field `phones` is changed from `LABEL_REPEATED` to `LABEL_OPTIONAL`.\n"
+                + "message_v1beta1.proto L22: An existing field `single` is moved out of One-of.\n"
+                + "message_v1.proto L18: An existing field `type` is removed.\n",
+            )
+
+    def test_single_directory_service(self):
+        with patch("sys.stdout", new=StringIO()):
+            runner = CliRunner()
+            result = runner.invoke(
+                detect,
+                [
+                    "test/testdata/protos/service/v1",
+                    "test/testdata/protos/service/v1beta1",
+                    "--human_readable_message",
+                ],
+            )
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                "service_v1.proto L11: An existing rpc method `shouldRemove` is removed.\n"
+                + "service_v1.proto L21: An existing field `page_size` is removed.\n"
+                + "service_v1.proto L22: An existing field `page_token` is removed.\n"
+                + "service_v1.proto L26: An existing field `content` is removed.\n"
+                + "service_v1.proto L27: An existing field `next_page_token` is removed.\n"
+                + "service_v1beta1.proto L7: Input type of an existing method `Foo` is changed from `FooRequest` to `FooRequestUpdate`.\n"
+                + "service_v1beta1.proto L7: Output type of an existing method `Foo` is changed from `FooResponse` to `FooResponseUpdate`.\n"
+                + "service_v1beta1.proto L9: The request streaming type of an existing method `Bar` is changed.\n"
+                + "service_v1beta1.proto L9: The response streaming type of an existing method `Bar` is changed.\n"
+                + "service_v1beta1.proto L11: The paginated response of an existing method `paginatedMethod` is changed.\n",
+            )
+
+    def test_single_directory_service_annotation(self):
+        with patch("sys.stdout", new=StringIO()):
+            runner = CliRunner()
+            result = runner.invoke(
+                detect,
+                [
+                    "test/testdata/protos/service_annotation/v1",
+                    "test/testdata/protos/service_annotation/v1beta1",
+                    "--human_readable_message",
+                ],
+            )
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                "service_annotation_v1beta1.proto L14: An existing http method of google.api.http annotation is changed for method `Foo`.\n"
+                + "service_annotation_v1beta1.proto L14: An existing http method URI of google.api.http annotation is changed for method `Foo`.\n"
+                + "service_annotation_v1beta1.proto L18: An existing method_signature for method `Foo` is changed from `content` to `error`.\n"
+                + "service_annotation_v1beta1.proto L18: An existing method_signature for method `Foo` is changed from `error` to `content`.\n"
+                + "service_annotation_v1beta1.proto L22: An existing http method body of google.api.http annotation is changed for method `Bar`.\n"
+                + "service_annotation_v1beta1.proto L26: The metadata_type of an existing LRO operation_info annotation for method `Bar` is changed from `FooMetadata` to `FooMetadataUpdate`.\n"
+                + "service_annotation_v1.proto L40: An existing message `FooMetadata` is removed.\n",
             )
 
     def test_mutiple_directories(self):
@@ -46,8 +116,8 @@ class CliDetectTest(unittest.TestCase):
             result = runner.invoke(
                 detect,
                 [
-                    "test/testdata/protos/example/enum_v1,test/testdata/protos/example/message_v1",
-                    "test/testdata/protos/example/enum_v1beta1,test/testdata/protos/example/message_v1beta1",
+                    "test/testdata/protos/enum/v1,test/testdata/protos/message/v1",
+                    "test/testdata/protos/enum/v1beta1,test/testdata/protos/message/v1beta1",
                     "--human_readable_message",
                 ],
             )
