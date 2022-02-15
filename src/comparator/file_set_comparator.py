8391e6b5ba762c1b272a11d6254c054841c75319
Viacheslav Rostovtsev <virost@google.com>
2022-02-14 18:14:56 -0800

    fixup

--- src/comparator/file_set_comparator.py
+++ src/comparator/file_set_comparator.py
@@ -80,11 +80,15 @@ class FileSetComparator:
                 transformed_option_value_update = {}
                 for namespace in per_language_options_original:
                     if namespace:
-                        transformed_name = self._get_version_update_name(str(namespace).lower())
+                        transformed_name = self._get_version_update_name(
+                            str(namespace).lower()
+                        )
                         transformed_option_value_original[transformed_name] = namespace
                 for namespace in per_language_options_update:
                     if namespace:
-                        transformed_option_value_update[str(namespace).lower()] = namespace
+                        transformed_option_value_update[
+                            str(namespace).lower()
+                        ] = namespace
 
                 for namespace in set(transformed_option_value_original.keys()) - set(
                     transformed_option_value_update.keys()
