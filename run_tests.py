893cb7100b8081494cbea69a8f4ca7795f5ccb77
Takashi Matsuo <tmatsuo@google.com>
2022-08-30 14:48:34 -0700

    chore: lint fix

--- run_tests.py
+++ run_tests.py
@@ -16,9 +16,9 @@ import os
 import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 
 if __name__ == "__main__":
-    all_tests = unittest.TestLoader().discover('test')
+    all_tests = unittest.TestLoader().discover("test")
     unittest.TextTestRunner().run(all_tests)
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="proto-breaking-change-detector",
-    packages=find_packages(where='src'),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     version="0.2",
     install_requires=[
