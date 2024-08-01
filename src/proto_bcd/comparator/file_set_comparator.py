1cfe118ff57f543820ac0fc7c77ed2cd9dd7ecd1
Noah Dietz <ndietz@google.com>
2024-08-01 15:51:14 +0000

    reformat

--- src/proto_bcd/comparator/file_set_comparator.py
+++ src/proto_bcd/comparator/file_set_comparator.py
@@ -305,9 +305,9 @@ class FileSetComparator:
             num_original = len(patterns_original)
             num_update = len(patterns_update)
             # This check is for detecting changes to the order of the patterns
-            # which is considered a breaking change. 
+            # which is considered a breaking change.
             # For more information, see <link to issue>.
-            # Check if a new pattern is inserted rather than appended or 
+            # Check if a new pattern is inserted rather than appended or
             # if the order of patterns has changed in place.
             if (
                 num_original < num_update
